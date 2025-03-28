673f3bdcf574ec1cfdd8372e621b8b8629161d27
Astawus zking4ever <yanosastawus@gmail.com>
2025-03-28 04:52:40 -0700

    functions added and stopwatch working

--- stopwatch/script.js
+++ stopwatch/script.js
@@ -24,9 +24,20 @@ _('stop').addEventListener('click',function(){
     }
 });
 
-
 function update(){
     elapseTime = Date.now() - startTime;
-    elapseTimeInHour = elapseTime / (1000);
-    _('display').innerHTML = elapseTimeInHour;
+    elapseTimeInSecond = Math.floor(elapseTime / (1000))%60;
+    elapseTimeInMinute = Math.floor(elapseTime / (1000*60))%60;
+    elapseTimeInHour = Math.floor(elapseTime / (1000*60*60));
+    _('display').innerHTML = elapseTimeInHour +":"+ elapseTimeInMinute +":"+ elapseTimeInSecond+":"+elapseTime;
 }
+
+//function to restart
+
+_('reset').addEventListener('click',function(){
+    _('display').innerHTML= "00:00:00:00";
+    startTime =0;
+    elapseTime =0;
+    isRunning =false;
+    clearInterval(intervalId);
+})
