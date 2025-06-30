147672d6f9ec05cdca3925b70b11646c65fd2881
Astawus <yanosastawus@gmail.com>
2025-06-30 11:43:34 +0300

    added memories and member profile and made the navbar responsible for device

--- friendship/script.js
+++ friendship/script.js
@@ -1,6 +1,14 @@
 var see =document.getElementById('see_more');
+var less =document.getElementById('see_less');
 see.addEventListener('click',function(){
-    for(var i=0; i<10;i++){
-    document.getElementsByClassName('members_container')[0].innerHTML += document.getElementsByClassName('member')[0].outerHTML;
-}
+    var prevheight = parseInt(window.getComputedStyle(document.getElementsByClassName('members_container')[0]).height);
+    document.getElementsByClassName('members_container')[0].style.height = 2*prevheight+"px";
+    see.style.right = "60%";
+    less.style.display = "block";
+});
+less.addEventListener('click',function(){
+    var prevheight = parseInt(window.getComputedStyle(document.getElementsByClassName('members_container')[0]).height);
+    document.getElementsByClassName('members_container')[0].style.height = prevheight/2 +"px";
+    see.style.right = "60%";
+    less.style.display = "block";
 });
